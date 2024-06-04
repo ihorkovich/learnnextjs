016a90f3b34e98f49f415d07398f10bfa80a3eb5
ihorkovich <igorzzz715@gmail.com>
2024-06-04 22:02:35 +0300

    second commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -38,7 +38,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-blue-600': pathname === link.href, // mapping all links. Each link has its own href. we get actual url on which page user is and compare it to hrefs from all links and add certain classes to link which has the same ref
               },
             )}
           >
